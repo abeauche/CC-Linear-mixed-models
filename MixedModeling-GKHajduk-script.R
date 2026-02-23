c269dfc399651946853623d452a6110c879953ef
Alex Beauchemin <abeauche@gmail.com>
2026-02-23 11:32:14 -0800

    Completed tutorial

--- MixedModeling-GKHajduk-script.R
+++ MixedModeling-GKHajduk-script.R
@@ -105,18 +105,24 @@ summary(mountain.lm)
 
 ###----- Mixed effects models -----###
 
+library(lme4)
 
 
 ##----- First mixed model -----##
 
 ### model
+mixed.lmer <- lmer(testScore ~ bodyLength2 + (1|mountainRange), data = dragons)
 
 ### plots
+plot(mixed.lmer)  # looks alright, no patterns evident
+qqnorm(resid(mixed.lmer))
+qqline(resid(mixed.lmer))  # points fall nicely onto the line - good!
 
 ### summary
+summary(mixed.lmer)
 
 ### variance accounted for by mountain ranges
-
+339.7/(339.7 + 223.8)  # ~60 %
 
 
 ##-- implicit vs explicit nesting --##
@@ -125,22 +131,101 @@ head(dragons)  # we have site and mountainRange
 str(dragons)  # we took samples from three sites per mountain range and eight mountain ranges in total
 
 ### create new "sample" variable
+dragons <- within(dragons, sample <- factor(mountainRange:site)) # create variable that is explicitely nested
 
 
 ##----- Second mixed model -----##
 
 ### model
+mixed.lmer2 <- lmer(testScore ~ bodyLength2 + (1|mountainRange) + (1|sample), data = dragons)  # the syntax stays the same, but now the nesting is taken into account
+# can use either (1|mountainRange/site) or even (1|mountainRange) + (1|mountainRange:site) but more advisable to ensure explicit naming within variables
 
 ### summary
+summary(mixed.lmer2)
 
 ### plot
+(mm_plot <- ggplot(dragons, aes(x = bodyLength, y = testScore, colour = site)) +
+    facet_wrap(~mountainRange, nrow=2) +   # a panel for each mountain range
+    geom_point(alpha = 0.5) +
+    theme_classic() +
+    geom_line(data = cbind(dragons, pred = predict(mixed.lmer2)), aes(y = pred), size = 1) +  # adding predicted line from mixed model 
+    theme(legend.position = "none",
+          panel.spacing = unit(2, "lines"))  # adding space between panels
+)
+# all of these have the same slope because we have only introduced random intercept models
 
+
+##----- Third mixed model -----##
+
+### model
+mixed.ranslope <- lmer(testScore ~ bodyLength2 + (1 + bodyLength2|mountainRange/site), data = dragons) 
+
+### summary
+summary(mixed.ranslope)
+
+### plot
+(mm_plot <- ggplot(dragons, aes(x = bodyLength, y = testScore, colour = site)) +
+    facet_wrap(~mountainRange, nrow=2) +   # a panel for each mountain range
+    geom_point(alpha = 0.5) +
+    theme_classic() +
+    geom_line(data = cbind(dragons, pred = predict(mixed.ranslope)), aes(y = pred), size = 1) +  # adding predicted line from mixed model 
+    theme(legend.position = "none",
+          panel.spacing = unit(2, "lines"))  # adding space between panels
+)
+
+
+##----- Presenting model results -----##
+
+library(ggeffects)  # install the package first if you haven't already, then load it
+
+# Extract the prediction data frame
+pred.mm <- ggpredict(mixed.lmer2, terms = c("bodyLength2"))  # this gives overall predictions for the model
+
+# Plot the predictions 
+(ggplot(pred.mm) + 
+    geom_line(aes(x = x, y = predicted)) +          # slope
+    geom_ribbon(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), 
+                fill = "lightgrey", alpha = 0.5) +  # error band
+    geom_point(data = dragons,                      # adding the raw data (scaled values)
+               aes(x = bodyLength2, y = testScore, colour = mountainRange)) + 
+    labs(x = "Body Length (indexed)", y = "Test Score", 
+         title = "Body length does not affect intelligence in dragons") + 
+    theme_minimal()
+)
+
+# Plot predictions according to different levels of random effects
+ggpredict(mixed.lmer2, terms = c("bodyLength2", "mountainRange"), type = "random") %>% # type argument updated from re to random
+  plot() +
+  labs(x = "Body Length", y = "Test Score", title = "Effect of body size on intelligence in dragons") + 
+  theme_minimal()
+
+# Visualize departures from overall model
+library(sjPlot)
+
+# Visualise random effects 
+(re.effects <- plot_model(mixed.ranslope, type = "re", show.values = TRUE))
+
+# show summary
+summary(mixed.ranslope)
+
+# tables
+library(stargazer)
+
+stargazer(mixed.lmer2, type = "text",
+          digits = 3,
+          star.cutoffs = c(0.05, 0.01, 0.001),
+          digit.separator = "")
 
 
 ##----- Model selection for the keen -----##
 
 ### full model
+full.lmer <- lmer(testScore ~ bodyLength2 + (1|mountainRange) + (1|sample), 
+                  data = dragons, REML = FALSE)
 
 ### reduced model
+reduced.lmer <- lmer(testScore ~ 1 + (1|mountainRange) + (1|sample), 
+                     data = dragons, REML = FALSE)
 
 ### comparison
+anova(reduced.lmer, full.lmer)  # the two models are not significantly different
